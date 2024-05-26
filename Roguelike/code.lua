c32d24f1c64b3434c1b887213b9d0827438b0de4
Boumety <162808181+Boumety@users.noreply.github.com>
2024-05-26 19:03:41 +0200

    Create code.lua

---
+++ Roguelike/code.lua
@@ -0,0 +1,14 @@
+--[[
+Here is the code for the game, Copy&Paste it in one of your world to start coding (because it's currently not possible to code in team on Cubzh)
+Please put comments for the other coders.
+Do not use the world editor.
+]]--
+
+Config = {
+}
+
+Client.OnStart = function()
+end
+
+Client.Tick = function(dt)
+end
